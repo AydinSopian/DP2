1598f2644c9829b0b0f435303e37d884647cc6da
Mobi-Ayub <mobiayub@gmail.com>
2018-10-15 20:35:49 +0800

    Added dbConnect class
    This class creates an object that connects to the database, then runs queries into the database.

---
+++ DP2/DP2/dbConnect.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace DP2
+{
+    class dbConnect
+    {
+        public dbConnect()
+        {
+
+        }
+
+        public void runQuery(string queryEntered)
+        {
+            string query = queryEntered;
+
+            if (query == "")
+            {
+                Console.WriteLine("Please insert some SQL query");
+                return;
+            }
+
+            string MySQLConnectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=kuhchingtest;sslmode=none";
+
+            MySqlConnection databaseConnection = new MySqlConnection(MySQLConnectionString);
+
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+
+            try
+            {
+                databaseConnection.Open();
+
+                MySqlDataReader myReader = commandDatabase.ExecuteReader();
+
+                if (myReader.HasRows)
+                {
+                    Console.WriteLine("Your query generated results, please see the console");
+                    while (myReader.Read())
+                    {
+                        for (int x = 0; x < myReader.FieldCount; x++)
+                        {
+                            Console.WriteLine(myReader.GetString(x) + " - ");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Query successfully executed");
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Query error " + e.Message);
+            }
+        }
+    }
+}
